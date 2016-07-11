6020468d919043e82f5a2f19b39bb4882f9891ac
qholness <qholness@gmail.com>
2016-07-11 16:39:48 -0500

    Update scrubble.py

--- scrubble.py
+++ scrubble.py
@@ -13,16 +13,16 @@ def I_am_a_scrub(scrubList):
 			if scrubList[i] > scrubList[i+1]:
 				scrubList[i], scrubList[i+1] = swap(scrubList[i], scrubList[i+1])
 	
-	if check(scrubList)==False:
-		main(scrubList)
+	if scrub_check(scrubList)==False:
+		I_am_a_scrub(scrubList)
 	
 	scrubList += ["What a scrub"]
 	
 	return scrubList
 
 swap = lambda a, b: (b, a)
-
-def check(scrubList):
+scrubby_list = lambda: int(time.time() % 2)
+def scrub_check(scrubList):
 	'''
 	Checks list elements to ensure it's in order.
 	I felt like using "sorted" was cheating.
@@ -31,6 +31,5 @@ def check(scrubList):
 		for n in range(i+1, len(scrubList)):
 			if scrubList[i] > scrubList[n]:
 				return False
-def scrubby_list():
-	return int(time.time() % 2)
+
 '''Runtime is O(n)+"Scrubs"'''
