2eff7dce3f7aac37548158016c297bfba0bb3890
qholness <qholness@gmail.com>
2016-07-11 17:05:21 -0500

    Update scrubble.py

--- scrubble.py
+++ scrubble.py
@@ -1,36 +1,55 @@
 import time
-def I_am_a_scrub(scrubList):
+class scrub_sorter(object):
 	'''
-	Main operation
+	This is a bubble sorter for scrubs.
+	Only use this is you or someone near you is a scrub.
+	Sometimes it works... Sometimes it won't.
+	Don't be disappoint scrub. Git Gud.
 	'''
-	assert isinstance(scrubList, (tuple, list))
-	
-	if isinstance(scrubList, tuple):
-		scrubList = list(scrubList)
-	
-	if scrubby_list(): #Good luck scrub
-		for i in range(len(scrubList)-1):
-			if scrubList[i] > scrubList[i+1]:
-				scrubList[i], scrubList[i+1] = swap(scrubList[i], scrubList[i+1])
-	
-	if scrub_check(scrubList)==False:
-		I_am_a_scrub(scrubList)
-	
-	scrubList += ["What a scrub"]
-	
-	return scrubList
+	def __init__(self, scrubList):
+		self.scrubList = scrubList
+	def scrubbleSort(self):
+		'''
+		Main operation
+		'''
+		assert isinstance(self.scrubList, (tuple, list))
+		
+		if isinstance(self.scrubList, tuple):
+			self.scrubList = list(self.scrubList)
+		
+		if self.scrubTime(): #Good luck scrub
+			for i in range(len(self.scrubList)-1):
+				if self.scrubList[i] > self.scrubList[i+1]:
+					self.scrubList[i], self.scrubList[i+1] = self.scrubbySwap(self.scrubList[i], self.scrubList[i+1])
+		
+		if self.scrub_check()==False:
+			self.scrubbleSort()
+		
+		return self.scrubList + ["What a scrub"]
 
-swap = lambda a, b: (b, a)
-scrubby_list = lambda: int(time.time() % 2)
-
-def scrub_check(scrubList):
-	'''
-	Checks list elements to ensure it's in order.
-	I felt like using "sorted" was cheating.
-	'''
-	for i in range(len(scrubList)-1):
-		for n in range(i+1, len(scrubList)):
-			if scrubList[i] > scrubList[n]:
-				return False
+	def scrubbySwap(self, a, b): return (b, a)
+	def scrubTime(self): return int(time.time() % 2)
+	def scrub_check(self):
+		'''
+		Checks list elements to ensure it's in order.
+		I felt like using "sorted" was cheating.
+		'''
+		for i in range(len(self.scrubList)-1):
+			for n in range(i+1, len(self.scrubList)):
+				if self.scrubList[i] > self.scrubList[n]:
+					return False
 
 '''Runtime is O(n)+"Scrubs"'''
+def main():a
+	scrubbyObject = scrub_sorter(input("Give me a list you scrub: ")).split()
+	for i in list:
+		if isintance(i, str):
+			print("GFTO")
+			return 1
+	try: 
+		sortedList = scrubbyObject.scrubbleSort()
+		print(sortedList)
+	except RecursionError: 
+		print("Fuck your list homey")
+if __name__ == '__main__':
+	main()
