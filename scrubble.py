f7e17ef0b528c85eacba91fcfc7d0255a61447f4
qholness <qholness@gmail.com>
2016-07-12 15:30:31 -0500

    Update scrubble.py

--- scrubble.py
+++ scrubble.py
@@ -5,7 +5,7 @@ from time import time
 
 class scrubbleSort(object):
 	'''
-	This is a bubble sorter for scrubs.
+	This is a bubble sorter for scrubs (a scrubble sorter if you will).
 	Only use this is you or someone near you is a scrub.
 	Sometimes it works... Sometimes it won't.
 	Don't be disappoint scrub. Git Gud.
@@ -30,7 +30,7 @@ class scrubbleSort(object):
 		if self.scrubCheck()==False:
 			self.scrubbleSort()
 		
-		return self.scrubList + ["What a scrub"]
+		return self.scrubList + ["What a scrub"]*len(self.scrubList) #As in, you probably shouldn't be sorting infinitely long lists in Python homey
 
 	def scrubbySwap(self, a, b): return (b, a)
 	
@@ -39,14 +39,17 @@ class scrubbleSort(object):
 	def scrubCheck(self):
 		'''
 		Checks list elements to ensure it's in order.
-		I felt like using "sorted" was cheating.
+		Using "sorted" is for srubz
 		'''
 		for i in range(len(self.scrubList)-1):
 			for n in range(i+1, len(self.scrubList)):
 				if self.scrubList[i] > self.scrubList[n]:
 					return False
 
-'''Runtime is O(n)+"Scrubs"'''
+'''
+Runtime is O(n)+"Scrubs"
+Example usage below:
+'''
 def main():
 	
 	scrubbyObject = scrubbleSort(input("Give me a list you scrub: ")).split()
